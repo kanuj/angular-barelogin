da4e96c23fbf394a70cf50293609394867e74969
Kanuj Bhatnagar <kanujbhatnagar@gmail.com>
2017-11-29 14:44:29 +0530

    Minor changes to variable names and getItem returns either string | null

--- src/app/storage-adapter.ts
+++ src/app/storage-adapter.ts
@@ -1,27 +1,28 @@
+import { Injectable } from '@angular/core';
 /**
  * Storage adapter override class
  * Uses localStorage for now, but can be changed to anything in the future
  */
-
+@Injectable()
 export class StorageAdapter {
 
-    private _adapter;
+    private storage: Storage;
 
     constructor() {
-        this._adapter = localStorage;
+        this.storage = window.localStorage;
     }
 
-    getItem(key: any): any {
-        return this._adapter.getItem(key) || null;
+    getItem(key: any): string | null {
+        return this.storage.getItem(key) || null;
     }
 
     setItem(key: any, value: any): boolean {
-        try { this._adapter.setItem(key, value); return true; }
+        try { this.storage.setItem(key, value); return true; }
         catch (err) { return false; }
     }
 
     removeItem(key: any): boolean {
-        try { this._adapter.removeItem(key); return true; }
+        try { this.storage.removeItem(key); return true; }
         catch (err) { return false; }
     }
 }
